49546d20bca65f8334e6c1d38853566cd150efc8
Vincenzo Chianese <vincenz.chianese@yahoo.it>
2016-04-05 13:36:12 +0200

    Look for new grunt-cli location

--- lib/grunt.sh
+++ lib/grunt.sh
@@ -4,6 +4,9 @@ run_grunt() {
     if [ -f $build_dir/node_modules/grunt-cli/bin/grunt ]; then
       $build_dir/node_modules/grunt-cli/bin/grunt heroku
     else
+      if [ -f $build_dir/node_modules/grunt/node_modules/grunt-cli/bin/grunt ]; then
+        $build_dir/node_modules/grunt/node_modules/grunt-cli/bin/grunt heroku
+    else
       $build_dir/node_modules/grunt/bin/grunt heroku
     fi
   else
